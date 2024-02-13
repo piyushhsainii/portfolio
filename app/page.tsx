cedadc789d88a0c8687da9e4c308d2cfeaf820e3
Piyush <sainipiyush8860@gmail.com>
2024-02-13 20:55:03 +0530

    Added Git calendar | Project section

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className='overlay'></div>
         <video src="/bg.mp4" loop autoPlay muted className="h-[100%] w-[100%] object-cover " ></video>
         <div className='content  ' >
-            <div className='w-[640px]  m-auto border border-white  h-[auto] ' >
+            <div className='w-[640px]  m-auto border-[0.05px] border-opacity-5 border-white  h-[auto] ' >
               <Content />    
             </div>
         </div>
