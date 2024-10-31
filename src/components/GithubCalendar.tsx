631e7bb5f5999854a61d0504afa64a51e8fb45df
Piyush Saini <134310734+piyushhsainii@users.noreply.github.com>
2024-10-31 13:10:03 +0530

    githubCalendar

--- src/components/GithubCalendar.tsx
+++ src/components/GithubCalendar.tsx
@@ -1,17 +1,14 @@
 "use client"
-import React, { Suspense } from 'react'
 import GitHubCalendar from 'react-github-calendar';
 
 
 const GitHubCalendarComponent = () => {
     return (
         <div className='    md:w-[720px]     ' >
-            <Suspense fallback="Loadin.." >
-                <GitHubCalendar
-                    username={'piyushhsainii'}
-                    blockSize={9}
-                />
-            </Suspense>
+            <GitHubCalendar
+                username={'piyushhsainii'}
+                blockSize={9}
+            />
         </div>
     )
 }
